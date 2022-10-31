14e4e03512791042f64bde3de0792fa279f8f88a
zadorozhnyayatatyana <zadorozhnyayatatyana@gmail.com>
2022-10-31 23:01:06 +0200

    HW1 upgrade

--- HW1/main.js
+++ HW1/main.js
@@ -2,37 +2,7 @@ let variable1 = 'hello', variable2 = 'owu', variable3 = 'com', variable4 = 'ua',
     variable7 = -999, variable8 = 123, variable9 = 3.14, variable10 = 2.7, variable11 = 16, variable12 = true,
     variable13 = false;
 
-console.log(variable1);
-console.log(variable2);
-console.log(variable3);
-console.log(variable4);
-console.log(variable5);
-console.log(variable6);
-console.log(variable7);
-console.log(variable8);
-console.log(variable9);
-console.log(variable10);
-console.log(variable11);
-console.log(variable12);
-console.log(variable13);
-
-alert(variable1);
-alert(variable2);
-alert(variable3);
-alert(variable4);
-alert(variable5);
-alert(variable6);
-alert(variable7);
-alert(variable8);
-alert(variable9);
-alert(variable10);
-alert(variable11);
-alert(variable12);
-alert(variable13);
-
-document.write(`${variable1} <br> ${variable2} <br> ${variable3} <br> ${variable4} <br> ${variable5} <br> 
-${variable6} <br> ${variable7} <br> ${variable8} <br> ${variable9} <br> ${variable10} <br> ${variable11} <br> 
-${variable12} <br> ${variable13}`);
+console.log(`${variable1}\n${variable2}\n${variable3}\n${variable4}\n${variable5}\n${variable6}\n${variable7}\n${variable8}\n${variable9}\n${variable10}\n${variable11}\n${variable12}\n${variable13}`);
 
 let firstName = 'Тетяна', middleName = 'Миколаївна', lastName = 'Задорожня';
 person = lastName + ' ' + firstName + ' ' + middleName;
